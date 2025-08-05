4cec34249517f1f28529f069cc30e94bcab7ded6
Shantel <Shantelgray24@gmail.com>
2025-08-05 11:52:40 -0400

    Creating the migration and seeds data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,16 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, "application/json"
 
   # Add your routes here
+   get '/monsters' do
+    monsters = Monster.all
+    monsters.to_json
+  end
+
+  get '/monsters/:id' do
+    monsters = Monster.find(params[:id])
+    monster.to_json(include: :mystery)
+  end
+
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
