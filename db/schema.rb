a20c98bb077acf81f43cab27ba611b1e84e008fb
Shantel <Shantelgray24@gmail.com>
2025-08-07 15:10:30 -0400

    Updated the Delete Error

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_08_05_145036) do
-
+ActiveRecord::Schema[8.0].define(version: 2025_08_05_145036) do
   create_table "monsters", force: :cascade do |t|
     t.string "name"
     t.string "disguise_as"
-    t.integer "mystery_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.integer "mystery_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["mystery_id"], name: "index_monsters_on_mystery_id"
   end
 
   create_table "mysteries", force: :cascade do |t|
     t.string "title"
     t.string "location"
-    t.datetime "date_reported"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "date_reported", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "monsters", "mysteries"
